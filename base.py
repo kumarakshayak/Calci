b314e187640cb578a60959923193e0e414e50e1e
akshaykumara <aksh6239san@gmail.com>
2024-01-24 13:13:05 +0530

    adding multiply

--- base.py
+++ base.py
@@ -1,4 +1,5 @@
 from add_func import addClass
+from multiply_func import multiplyClass
 class calculator:
 
     def __init__(self,number1,number2):
@@ -6,11 +7,13 @@ class calculator:
         self.num2=number2
     def add(self):
         return addClass.add(self.num1,self.num2)
-    
+    def multiply(self):
+                 return multiplyClass.multiply(self.num1,self.num2)
     
 if __name__=="__main__":
     a=10
     b=2
     cur_obj=calculator(a,b)
     print(cur_obj.add())
+    print(cur_obj.multiply())
 
---
+++ multiply_func.py
@@ -0,0 +1,3 @@
+class multiplyClass:
+    def multiply(num1,num2):
+        return num1*num2
