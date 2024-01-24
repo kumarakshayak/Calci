149771d6f36ff41841c828665d36f0d25430e084
akshaykumara <aksh6239san@gmail.com>
2024-01-24 13:39:22 +0530

    adding the common func for multi and add

---
+++ add_and_multi.py
@@ -0,0 +1,5 @@
+class add_multi:
+    def add(num1,num2):
+        return num1+num2
+    def multi(num1,num2):
+        return num1*num2
--- base.py
+++ base.py
@@ -1,11 +1,19 @@
+from add_and_multi import add_multi
+
 class calculator:
 
     def __init__(self,number1,number2):
         self.num1=number1
         self.num2=number2
+    def add(self):
+        return add_multi.add(self.num1,self.num2)
+    def multiply(self):
+                 return add_multi.multi(self.num1,self.num2)
     
 if __name__=="__main__":
     number1=10
-    b=2
-    cur_obj=calculator(a,b)
-    
+    number2=2
+    cur_obj=calculator(number1,number2)
+    print(cur_obj.add())
+    print(cur_obj.multiply())
+
