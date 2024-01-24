c706a11ce038fe43193060897ec57ec9530a3e6c
akshaykumara <aksh6239san@gmail.com>
2024-01-24 12:52:45 +0530

    first commit adding calculator class

---
+++ base.py
@@ -0,0 +1,11 @@
+class calculator:
+
+    def __init__(self,number1,number2):
+        self.num1=number1
+        self.num2=number2
+    
+if __name__=="__main__":
+    a=10
+    b=2
+    cur_obj=calculator(a,b)
+    
